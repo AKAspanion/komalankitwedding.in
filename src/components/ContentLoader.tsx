58e62193a493d8016971e5efab0090278be04d14
akaspanion <panditankit1995@gmail.com>
2024-10-03 22:25:04 +0530

    build fixes

--- src/components/ContentLoader.tsx
+++ src/components/ContentLoader.tsx
@@ -1,8 +1,6 @@
 import { FC } from "react";
 
-interface ContentLoaderProps {}
-
-const ContentLoader: FC<ContentLoaderProps> = ({}) => {
+const ContentLoader: FC = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
       <div className="loading loading-ring loading-lg"></div>
